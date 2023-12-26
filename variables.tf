dff119c69b3426f60e1062e3fd6bd0100ebbc5e8
José Enrique <politeles@users.noreply.github.com>
2023-12-26 20:22:05 +0100

    deleted vars for public and private subnets

--- variables.tf
+++ variables.tf
@@ -22,13 +22,3 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
-
-variable "public_subnets" {
-  description = "List of CIDR blocks for subnets"
-  type        = list(string)
-}
-
-variable "private_subnets" {
-  description = "List of CIDR blocks for subnets"
-  type        = list(string)
-}

ffebcb7727717947f3974afc74f988eed8319315
José Enrique <politeles@users.noreply.github.com>
2024-01-03 10:09:37 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -30,15 +30,15 @@ module "vpc" {
 
   name = var.vpc_name
 
-  cidr = var.vpc_cidr
-  azs  = local.azs
-  private_subnets     = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
-  public_subnets      = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 4)]
+  cidr            = var.vpc_cidr
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 4)]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
